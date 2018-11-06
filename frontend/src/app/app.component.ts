01c213867f6d388c77c75f2b265c7957aad34e95
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-11-05 22:20:37 -0700

    Working, needs controls and magic.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
+
+
+interface ApiResponse {
+  data: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'serverless-ipsum';
+  content;
+
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient) {
+
+    this.http.post<ApiResponse>('https://owdkhmj7j4.execute-api.us-west-2.amazonaws.com/dev/filler-text', {
+      type: 'Cat'
+    }).subscribe(body => {
+      console.log('Here!', body);
+      this.content = this.sanitizer.bypassSecurityTrustHtml(body.data)
+    })
+    ;
+  }
 }
